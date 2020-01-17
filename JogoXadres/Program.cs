d56d625f8d48a6a770fe809d6babc6fdd23cd914
Rei Almeida <reiguitarra@gmail.com>
2020-01-17 12:29:16 -0300

    Foi criado as classe Cor, peça e Tabuleiro, sem erro aqui

--- JogoXadres/Program.cs
+++ JogoXadres/Program.cs
@@ -8,9 +8,7 @@ namespace JogoXadres
         static void Main(string[] args)
         {
 
-            Posicao p = new Posicao(3,4);
-
-            Console.WriteLine("Posição : "+ p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
         }
     }
 }
---
+++ JogoXadres/tabuleiro/Peca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QuantMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            QuantMovimentos = 0;
+            Tab = tab;
+        }
+        public Peca()
+        {
+
+        }
+    }
+}
