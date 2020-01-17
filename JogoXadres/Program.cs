0046e8eebf47850f76b35ab55f309876284a43e7
Rei Almeida <reiguitarra@gmail.com>
2020-01-17 11:19:52 -0300

    Criado projeto Xadrez e classe tabuleiro

---
+++ JogoXadres/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+
+namespace JogoXadres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Posicao p = new Posicao(3,4);
+
+            Console.WriteLine("Posição : "+ p);
+        }
+    }
+}
